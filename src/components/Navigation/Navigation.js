aa280c8bfb592bf6ca2e35ec9a3138257c03675c
Hiep Ho <mikevoli221@yahoo.com>
2020-05-12 01:36:10 +0700

    update codes

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navigation = () => {
+    return (
+        <nav className = 'tr fs link dim black underline pa3 pointer'>
+            <p>Sign Out</p>
+        </nav>
+    )
+}
+
+export default Navigation;
