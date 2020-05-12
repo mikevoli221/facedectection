c7a7a2c442cf06b4a6fa4f0a0369014be37305fa
Hiep Ho <mikevoli221@yahoo.com>
2020-05-13 00:47:49 +0700

    update clarifai api integration

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,6 +6,7 @@ import ImageLinkForm from '../components/ImageLinkForm/ImageLinkForm'
 import Rank from '../components/Rank/Rank';
 import 'tachyons';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 
 const particlesOptions = {
   particles: {
@@ -19,7 +20,37 @@ const particlesOptions = {
   }
 }
 
+const app = new Clarifai.App({
+  apiKey: '953f4833c2c54483a912bed1b841bde5'
+ });
+ 
+
 class App extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      input : ''
+    }
+  }
+
+  onInputChange = (event) => {
+     console.log(event.target.value);
+  }
+
+  onButtonClick = (event) => {
+    console.log('button clicked')
+    app.models.initModel({id: Clarifai.GENERAL_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"})
+      .then(generalModel => {
+        return generalModel.predict("https://samples.clarifai.com/metro-north.jpg");
+      })
+      .then(response => {
+        let concepts = response['outputs'][0]['data']['concepts']
+        console.log(concepts);
+        console.log(response);
+      })
+  }
+
   render(){
     return (
       <div className="App">
@@ -28,7 +59,7 @@ class App extends React.Component {
         <Navigation/>
         <Logo/>
         <Rank/>
-        <ImageLinkForm/>
+        <ImageLinkForm onInputChange = {this.onInputChange} onButtonClick = {this.onButtonClick}/>
         
         {/*  
         <FaceRecognition/> 
